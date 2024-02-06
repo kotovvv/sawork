ce5bd1b21786ed3e8820894fd980d5ab78b06ffd
kotovvv <kotovvv@gmail.com>
2024-02-06 15:20:46 +0200

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue';
+import App from '@/App.vue';
+// import Router from '@/router/router.js';
+// import Store from '@/store/store.js';
+
+// createApp(App).use(Router).use(Store).mount('#app');
+createApp(App).mount('#app');
---
+++ resources/js/src/components/MainComponent.vue
@@ -0,0 +1,33 @@
+<template>
+	<div>
+		{{ test }}
+	</div>
+</template>
+<script>
+import axios from 'axios';
+export default {
+	name: 'MainComponent',
+
+	data() {
+		return {
+			test: 'test 111111111111111',
+		};
+	},
+
+	mounted() {
+		this.getWarehouse();
+	},
+
+	methods: {
+		getWarehouse() {
+			const vm = this;
+			axios
+				.get('/api/getWarehouse')
+				.then((res) => {
+					vm.test = res.data;
+				})
+				.catch((error) => console.log(error));
+		},
+	},
+};
+</script>
