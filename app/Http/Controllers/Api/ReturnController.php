591b8952465f6134049fe532e67251a39c37abf0
kotovvv <kotovvv@gmail.com>
2024-03-19 21:58:06 +0200

    doc wz

--- app/Http/Controllers/Api/ReturnController.php
+++ app/Http/Controllers/Api/ReturnController.php
@@ -24,33 +24,38 @@ class ReturnController extends Controller
     {
         $data = $request->all();
         $orderdata = trim($data['ordername']);
-        $order = [];
-        //      $order['info'] =  collect(DB::select('SELECT [IDOrder] ,[Number] ,[Created] ,[IDOrderType] FROM [dbo].[Orders] WHERE [IDWarehouse] = ' . (int) $data['warehouse'] . ' AND ([Number] = \'' . $orderdata . '\' OR [_OrdersTempString3] = \'' . $orderdata . '\' OR [_OrdersTempString2] = \'' . $orderdata . '\' OR [_OrdersTempDecimal1] = \'' . $orderdata . '\''))->first();
+        $res = [];
+        //      $res['info'] =  collect(DB::select('SELECT [IDOrder] ,[Number] ,[Created] ,[IDOrderType] FROM [dbo].[Orders] WHERE [IDWarehouse] = ' . (int) $data['warehouse'] . ' AND ([Number] = \'' . $orderdata . '\' OR [_OrdersTempString3] = \'' . $orderdata . '\' OR [_OrdersTempString2] = \'' . $orderdata . '\' OR [_OrdersTempDecimal1] = \'' . $orderdata . '\''))->first();
 
-        $order['info'] =  collect(DB::select('SELECT [IDOrder] ,[Number] ,cast ([Created] as date) Created ,[IDOrderType],con.[Nazwa] cName FROM [dbo].[Orders] ord
+        $res['order'] =  collect(DB::select('SELECT [IDOrder] ,[Number] ,cast ([Created] as date) Created ,[IDOrderType],con.[Nazwa] cName FROM [dbo].[Orders] ord
         LEFT JOIN [dbo].[Kontrahent] con ON con.[IDKontrahenta] = ord.[IDAccount]
          WHERE [IDWarehouse] = ' . (int) $data['warehouse'] . ' AND \'' . $orderdata . '\' IN (_OrdersTempString2, Number, _OrdersTempString1, _OrdersTempString3, CONVERT(NVARCHAR(255), _OrdersTempDecimal1))'))->first();
 
-        if ($order['info']) {
-            $order['products'] =
-                DB::select('SELECT [IDOrderLine]
-           ,[IDItem]
+        $res['wz'] = collect(
+            DB::select('SELECT [ID1] FROM [dbo].[DocumentRelations] WHERE [ID2] = ' . $res['order']->IDOrder . ' AND [IDType1] = 2')
+        )->first();
+
+        if ($res['wz']) {
+            $res['products'] =
+                DB::select('SELECT [IDTowaru]
+            ,[Ilosc]
+            ,[Wydano]
+           ,[CenaJednostkowa]
            ,tov.[Nazwa]
            ,tov.[KodKreskowy]
            ,tov.[_TowarTempString1]
            ,tov.[Zdjecie] img
-            ,[Quantity]
-       FROM [dbo].[OrderLines] ord
-       LEFT JOIN [dbo].[Towar]  tov ON ord.[IDItem] = tov.[IDTowaru]
-       WHERE [IDOrder] = ' . $order['info']->IDOrder);
+       FROM [dbo].[ElementRuchuMagazynowego] wz
+       LEFT JOIN [dbo].[Towar]  tov ON wz.[IDTowaru] = tov.[IDTowaru]
+       WHERE [IDRuchuMagazynowego] = ' . $res['wz']->ID1);
 
-            foreach ($order['products'] as $key => $product) {
+            foreach ($res['products'] as $key => $product) {
                 if ($product->img) {
-                    $order['products'][$key]->img =  base64_encode($product->img);
+                    $res['products'][$key]->img =  base64_encode($product->img);
                 }
             }
         }
-        return response($order);
+        return response($res);
     }
     /**
      * Display a listing of the resource.
