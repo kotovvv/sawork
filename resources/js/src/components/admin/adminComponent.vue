d61e5e46f37a38576a3b25aca154d5a640e7d662
kotovvv <kotovvv@gmail.com>
2024-05-14 16:29:31 +0300

    test

--- resources/js/src/components/admin/adminComponent.vue
+++ resources/js/src/components/admin/adminComponent.vue
@@ -1,12 +1,29 @@
 <template>
 	<v-app id="inspire">
-		<v-navigation-drawer :location="$vuetify.display.mobile ? 'bottom' : undefined">
+		<v-navigation-drawer
+			:location="$vuetify.display.mobile ? 'bottom' : undefined"
+			:rail="rail"
+			permanent
+			@click="rail = false"
+		>
 			<!-- menu -->
 			<v-list
 				:lines="false"
 				density="compact"
 				nav
 			>
+				<v-list-item
+					nav
+					prepend-avatar="/img/logo.webp"
+				>
+					<template v-slot:append>
+						<v-btn
+							icon="mdi-chevron-left"
+							variant="text"
+							@click.stop="rail = !rail"
+						></v-btn>
+					</template>
+				</v-list-item>
 				<v-list-item
 					v-for="(item, i) in items"
 					:key="i"
@@ -49,11 +66,14 @@ export default {
 	props: ['user'],
 	data: () => ({
 		drawer: true,
-		selectedItem: 0,
+		rail: true,
+
 		theMenu: 'dictionaryComponent',
 
-		items: [{ text: 'Podręczniki', name: 'dictionaryComponent', icon: 'mdi-list-box-outline' }],
-		items: [{ text: 'Test', name: 'test', icon: 'mdi-barcode-scan' }],
+		items: [
+			{ text: 'Podręczniki', name: 'dictionaryComponent', icon: 'mdi-list-box-outline' },
+			{ text: 'Test', name: 'test', icon: 'mdi-barcode-scan' },
+		],
 	}),
 	computed: {
 		setComponent() {
--- resources/js/src/components/admin/test.vue
+++ resources/js/src/components/admin/test.vue
@@ -1,37 +1,55 @@
 <template>
 	<v-container>
+		<v-card>
+			<v-card-text>
+				<v-text-field
+					v-model="imputCod"
+					label="Cod"
+					ref="cod"
+				></v-text-field>
+				Enter: {{ text }}<br />
+			</v-card-text>
+		</v-card>
+		<!-- @keyup.capture="handleKeypress" -->
 		<v-dialog
 			id="dialogProduct"
 			ref="dProduct"
 			v-model="dialogProduct"
 			transition="dialog-bottom-transition"
 			fullscreen
-			@keyup.capture="handleKeypress"
+			@paste="eventPaste"
 		>
 			<v-container>
 				<v-card>
 					<v-card-title class="mb-5 bg-grey-lighten-3">
 						<v-row>
-							<v-col>
-								Enter: {{ dialog_text }}<br />
-								Input: {{ imputCod }}
-							</v-col>
+							<v-col> </v-col>
 							<v-spacer></v-spacer>
 
 							<v-btn
 								icon="mdi-close"
-								@click="
-									imputCod = '';
-									dialog_text = '';
-								"
+								@click="dialogProduct = false"
 							></v-btn
 						></v-row>
 					</v-card-title>
 
-					<v-card-text> </v-card-text>
+					<v-card-text>
+						<v-text-field label="Cod"></v-text-field>
+
+						Enter: {{ dialog_text }}<br />
+						Input: {{ imputCod }}
+					</v-card-text>
 					<template v-slot:actions>
 						<v-spacer></v-spacer>
 						<!-- v-if="products.find((e) => e.qty > 0)" -->
+						<v-btn
+							@click="
+								imputCod = '';
+								dialog_text = '';
+							"
+						>
+							clear</v-btn
+						>
 					</template>
 				</v-card>
 			</v-container>
@@ -45,33 +63,15 @@ export default {
 
 	data() {
 		return {
-			dialogProduct: true,
-			dialog_text: '',
-			dialog_title: '',
+			text: '',
+
 			imputCod: '',
 		};
 	},
 
 	mounted() {},
 
-	methods: {
-		handleKeypress(event) {
-			// Check if the Enter key was pressed
-			if (event.key === 'Enter') {
-				// Execute the function with the accumulated input
-				this.dialog_text = this.imputCod;
-				// Clear the input field
-				this.imputCod = '';
-			} else {
-				// Append the current keystroke to the input
-				this.imputCod += event.key;
-			}
-		},
-
-		clear() {
-			this.imputCod = '';
-		},
-	},
+	methods: {},
 };
 </script>
 <style lang="scss">
