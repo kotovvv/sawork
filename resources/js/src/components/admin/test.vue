ca2a247fd6eba00254404d7f5b6e8f46294fc193
kotovvv <kotovvv@gmail.com>
2024-05-15 15:43:23 +0300

    remive description

--- resources/js/src/components/admin/test.vue
+++ resources/js/src/components/admin/test.vue
@@ -4,7 +4,7 @@
 		tabindex="0"
 		@keypress="handleKeyDown"
 	>
-		<v-card height="100vh">
+		<v-card height="90vh">
 			<v-card-text> You enter: {{ text }}<br /> </v-card-text>
 		</v-card>
 	</div>
@@ -24,27 +24,18 @@ export default {
 	},
 
 	methods: {
-		makeAny() {
-			this.text = this.inputCod.replace('undefined', '');
-		},
 		setFocus() {
 			this.text = '';
 			this.inputCod = '';
 			this.$refs.focusTarget.focus(); // Focus the target element
 		},
 		handleKeyDown(event) {
-			// Prevent default behavior (e.g., scrolling) if necessary
 			event.preventDefault();
 
 			if (event.key === 'Enter') {
-				// Execute the function with the accumulated input
 				this.text = this.imputCod.replace('undefined', '');
-
-				// Clear the input field
 				this.imputCod = '';
 			} else {
-				// Append the current keystroke to the input
-
 				this.imputCod += event.key;
 			}
 		},
