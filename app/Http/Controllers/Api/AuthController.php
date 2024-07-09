5bec8822b62445375d0b06ef34776126c9d970ef
kotovvv <kotovvv@gmail.com>
2024-07-09 21:24:07 +0300

    passwordHash

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 //use Laravel\Sanctum\HasApiTokens;
 
 // app/Http/Controllers/AuthController.php
@@ -14,8 +15,15 @@ class AuthController extends Controller
     {
         $credentials = $request->only('login', 'password');
 
-        if (Auth::attempt($credentials)) {
+        $passwordHash = $this->getPasswordHash($request->password);
+
+        // Load user from Uzytkownik table
+        $user = User::where('Login', $request->login)->first();
+
+        // Check that user exists and hashed passwords are the same
+        if ($user && $user->Haslo === $passwordHash) {
             $user = Auth::user();
+
             $token = $user->createToken('Personal Access Token')->accessToken;
 
             return response()->json([
@@ -25,5 +33,12 @@ class AuthController extends Controller
         } else {
             return response()->json(['error' => 'Unauthorized'], 401);
         }
+    }
+
+    public function getPasswordHash($password): string
+    {
+        $md5 = md5($password);
+        $unHex = hex2bin($md5);
+        return base64_encode($unHex);
     }
 }
