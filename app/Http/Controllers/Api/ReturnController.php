ec02dae765347efd77f6a4e6f943abe31984199b
kotovvv <kotovvv@ukr.net>
2024-07-16 16:18:02 +0300

    fix token to user

--- app/Http/Controllers/Api/ReturnController.php
+++ app/Http/Controllers/Api/ReturnController.php
@@ -11,10 +11,10 @@ class ReturnController extends Controller
 {
     public function getWarehouse(Request $request)
     {
-        $user = $request->user;
-        if ($user) {
 
-            $a_mag = DB::table('UprawnieniaDoMagazynow')->where('IDUzytkownika', $user->IDUzytkownika)->where('Uprawniony', 1)->pluck('IDMagazynu');
+        if (isset($request->user)) {
+            $user = $request->user;
+            $a_mag = collect(DB::table('UprawnieniaDoMagazynow')->where('IDUzytkownika', $user->IDUzytkownika)->where('Uprawniony', 1)->pluck('IDMagazynu'))->toArray();
 
             return DB::table('Magazyn')->select('IDMagazynu', 'Nazwa', 'Symbol')->whereIn('IDMagazynu', $a_mag)->get();
         }
