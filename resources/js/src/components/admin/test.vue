e0363fac61878bb35617484e72eee770c1d817cf
Kotov VV <kotovvv@gmail.com>
2024-09-20 13:57:29 +0200

    4

--- resources/js/src/components/admin/test.vue
+++ resources/js/src/components/admin/test.vue
@@ -1,21 +1,45 @@
 <template>
-	<div>
-		<get-pic />
+	<div
+		ref="focusTarget"
+		tabindex="0"
+		@keypress="handleKeyDown"
+	>
+		<v-card height="90vh">
+			<v-card-text> You enter: {{ text }}<br /> </v-card-text>
+		</v-card>
 	</div>
 </template>
 
 <script>
-import getPic from '../UI/getPic.vue';
-
 export default {
 	name: 'Test',
-	components: { getPic },
 	data() {
-		return {};
+		return {
+			text: '',
+			inputCod: '',
+		};
 	},
-	mounted() {},
+	mounted() {
+		this.setFocus();
+	},
 
-	methods: {},
+	methods: {
+		setFocus() {
+			this.text = '';
+			this.inputCod = '';
+			this.$refs.focusTarget.focus(); // Focus the target element
+		},
+		handleKeyDown(event) {
+			event.preventDefault();
+
+			if (event.key === 'Enter') {
+				this.text = this.imputCod.replace('undefined', '');
+				this.imputCod = '';
+			} else {
+				this.imputCod += event.key;
+			}
+		},
+	},
 };
 </script>
 <style lang="scss">
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,37 +1,15 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>FulStor</title>
+    @vite('resources/js/app.js')
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    {{-- @vite('resources/js/app.js') --}}
-    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
-    {{-- <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-<script src="{{ asset('js/app.js') }}" defer></script> --}}
-
-@production
-    @php $path = public_path('build\assets'); @endphp
-
-@if (file_exists($path))
-    @foreach (scandir($path) as $file)
-        @if (strpos($file, '.css'))
-            <link rel="stylesheet" href="{{ asset('build/assets/' . $file) }}">
-        @endif
-        @if (strpos($file, '.js'))
-            @push('scripts')
-                <script src="{{ asset('build/assets/' . $file) }}"></script>
-            @endpush()
-        @endif
-    @endforeach
-@endif
-@else
-@vite(['resources/css/app.css', 'resources/js/app.js'])
-@endproduction
 </head>
 <body>
 <div id="app"></div>
-@stack('scripts')
+
 </body>
 </html>
