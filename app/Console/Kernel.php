dc6b0ddddc0e598a6bc84df4100b6c9ef88bc0b3
Kotovvv <kotovvv@gmail.com>
2025-05-04 15:39:01 +0300

    feat(schedule): add email notification for specific warehouse location errors

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,6 +7,8 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use App\Http\Controllers\Api\SendPDF;
 use App\Http\Controllers\Api\importBLController;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\DB;
 
 class Kernel extends ConsoleKernel
 {
@@ -37,6 +39,46 @@ class Kernel extends ConsoleKernel
                     new importBLController();
                 }
             )->everyMinute();
+
+            $schedule->call(function () {
+                $results = \DB::select("
+                    SELECT
+                        (SELECT NrDokumentu
+                         FROM dbo.RuchMagazynowy r
+                         LEFT JOIN dbo.ElementRuchuMagazynowego e ON e.IDRuchuMagazynowego = r.IDRuchuMagazynowego
+                         WHERE e.IDElementuRuchuMagazynowego = z.IDElementuWZ) AS ndoc,
+                        z.IDElementuWZ,
+                        rm.NrDokumentu,
+                        rm.\"Data\",
+                        t.KodKreskowy,
+                        erm.*
+                    FROM dbo.ElementRuchuMagazynowego erm
+                    LEFT JOIN dbo.RuchMagazynowy rm ON erm.IDRuchuMagazynowego = rm.IDRuchuMagazynowego
+                    LEFT JOIN dbo.Towar t ON t.IDTowaru = erm.IDTowaru
+                    JOIN dbo.ZaleznosciPZWZ z ON z.IDElementuPZ = erm.IDElementuRuchuMagazynowego
+                    WHERE IDWarehouseLocation IN (
+                        SELECT Zniszczony
+                        FROM dbo.EMailMagazyn
+                        WHERE Zniszczony IS NOT NULL
+                    ) AND z.IDElementuWZ NOT IN (32378,230685,142812)
+                ");
+                //,173864
+                if (!empty($results)) {
+                    $emailData = [];
+                    foreach ($results as $row) {
+                        $emailData[] = [
+                            'ndoc' => $row->ndoc,
+                            'NrDokumentu' => $row->NrDokumentu,
+                        ];
+                    }
+
+                    \Mail::send([], [], function ($message) use ($emailData) {
+                        $message->to('kotovvv@gmail.com')
+                            ->subject('Ошибка в локациях')
+                            ->setBody('В следующих заказах: ' . json_encode($emailData), 'text/html');
+                    });
+                }
+            })->dailyAt('18:00');
         }
     }
 
