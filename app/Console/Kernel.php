18e0f4dba01f4da21525267d1281164226b20f33
Kotov VV <kotovvv@gmail.com>
2025-05-05 19:08:39 +0200

    fix(Kernel): uncomment instantiation of importBLController in schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -36,7 +36,7 @@ class Kernel extends ConsoleKernel
 
             $schedule->call(
                 function () {
-                    //new importBLController();
+                    new importBLController();
                 }
             )->everyMinute();
 
