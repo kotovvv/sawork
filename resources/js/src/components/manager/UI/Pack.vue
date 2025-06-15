f08d7078f7909c2076d6d41803cba771f0641f53
Kotovvv <kotovvv@gmail.com>
2025-06-15 15:19:54 +0300

    Enhance scanKod method to improve error handling and beep feedback; ensure order status is updated after scanning

--- resources/js/src/components/manager/UI/Pack.vue
+++ resources/js/src/components/manager/UI/Pack.vue
@@ -648,8 +648,10 @@ export default {
       this.message = "";
       this.message_error = "";
       this.test = "";
+      let find = false;
       this.productsOrder[0].products.forEach((product) => {
         if (product.KodKreskowy == this.imputCod) {
+          find = true;
           product.qty = parseInt(product.qty) + 1;
           if (typeof window !== "undefined" && window.AudioContext) {
             const ctx = new (window.AudioContext ||
@@ -664,48 +666,47 @@ export default {
               ctx.close();
             }, 150); // 150 ms beep
           }
-          this.imputCod = "";
-          this.test = "";
-          this.isOrderDone();
-          return true;
           //   this.message = "Zeskanowano kod: " + this.imputCod;
           //   this.snackbar = true;
         }
       });
       // this.snackbar = true;
+      if (!find) {
+        if (typeof window !== "undefined" && window.AudioContext) {
+          const ctx = new (window.AudioContext || window.webkitAudioContext)();
+          const gain = ctx.createGain();
+          gain.gain.setValueAtTime(0.5, ctx.currentTime);
+          gain.connect(ctx.destination);
 
-      if (typeof window !== "undefined" && window.AudioContext) {
-        const ctx = new (window.AudioContext || window.webkitAudioContext)();
-        const gain = ctx.createGain();
-        gain.gain.setValueAtTime(0.5, ctx.currentTime);
-        gain.connect(ctx.destination);
+          // Repeat beep: 3 short beeps
+          let beepCount = 0;
 
-        // Repeat beep: 3 short beeps
-        let beepCount = 0;
+          function beep() {
+            const oscillator = ctx.createOscillator();
+            oscillator.type = "square";
+            oscillator.frequency.setValueAtTime(220, ctx.currentTime);
+            oscillator.connect(gain);
+            oscillator.start();
+            setTimeout(() => {
+              oscillator.stop();
+              oscillator.disconnect();
+              beepCount++;
+              if (beepCount < 3) {
+                setTimeout(beep, 100); // пауза между бипами
+              } else {
+                setTimeout(() => ctx.close(), 200);
+              }
+            }, 150); // длительность бипа
+          }
+          beep();
 
-        function beep() {
-          const oscillator = ctx.createOscillator();
-          oscillator.type = "square";
-          oscillator.frequency.setValueAtTime(220, ctx.currentTime);
-          oscillator.connect(gain);
-          oscillator.start();
-          setTimeout(() => {
-            oscillator.stop();
-            oscillator.disconnect();
-            beepCount++;
-            if (beepCount < 3) {
-              setTimeout(beep, 100); // пауза между бипами
-            } else {
-              setTimeout(() => ctx.close(), 200);
-            }
-          }, 150); // длительность бипа
+          this.message_error = "Coś jest nie tak!!!";
         }
-        beep();
-
-        this.message_error = "Coś jest nie tak!!!";
-        this.imputCod = "";
-        this.test = "";
       }
+
+      this.imputCod = "";
+      this.test = "";
+      this.isOrderDone();
     },
     getOrderPackProducts(id) {
       this.message = "";
