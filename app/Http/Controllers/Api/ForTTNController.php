bc965ce15fbeed5336757c56d2b11aa7ea69dc15
Kotovvv <kotovvv@gmail.com>
2025-07-06 14:50:17 +0300

    Fix package description formatting in getForm method to replace special characters

--- app/Http/Controllers/Api/ForTTNController.php
+++ app/Http/Controllers/Api/ForTTNController.php
@@ -262,7 +262,7 @@ class ForTTNController extends Controller
 */
         $res['default_values']['fields']['insurance'] = $order->KwotaBrutto;
         $res['default_values']['fields']['reference_number'] = (int)$order->NumberBL;
-        $res['default_values']['fields']['package_description'] = $order->Number;
+        $res['default_values']['fields']['package_description'] = str_replace(['/', '-'], [' ', ''], $order->Number);
         if (!is_null($service)) {
             $res['default_values']['fields']['service'] = $service;
         }
