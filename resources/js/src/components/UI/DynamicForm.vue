8419bbc267762221f90ccbdba8d72b80b2fff695
www-data <www-data@fulstor.local>
2025-07-13 20:14:37 +0200

    Remove validation rules from DynamicForm fields and simplify save function

--- resources/js/src/components/UI/DynamicForm.vue
+++ resources/js/src/components/UI/DynamicForm.vue
@@ -15,7 +15,6 @@
             <v-radio-group
               v-model="fieldsData[field.id]"
               :name="field.id"
-              :rules="getFieldRules(field)"
               @keydown.enter.prevent="save"
             >
               <v-radio
@@ -36,7 +35,6 @@
                 :label="opt.title"
                 :value="opt.value"
                 :name="`${field.id}_${opt.value}`"
-                :rules="getFieldRules(field)"
                 multiple
                 hide-details="auto"
                 @keydown.enter.prevent="save"
@@ -58,7 +56,6 @@
                   :label="field.name"
                   readonly
                   :name="field.id"
-                  :rules="getFieldRules(field)"
                   @keydown.enter.prevent="save"
                 />
               </template>
@@ -80,7 +77,6 @@
               :hint="field.hint"
               persistent-hint
               :name="`package_${field.id}`"
-              :rules="getFieldRules(field)"
               :true-value="true"
               :false-value="false"
               @keydown.enter.prevent="save"
@@ -100,7 +96,6 @@
             :hint="field.hint"
             persistent-hint
             :name="field.id"
-            :rules="getFieldRules(field)"
             :true-value="true"
             :false-value="false"
             @keydown.enter.prevent="save"
@@ -223,20 +218,6 @@ function validateRequiredFields() {
 
 // Сохранение формы
 function save() {
-  // Проверяем валидность формы
-  if (!valid.value) {
-    return;
-  }
-
-  // Проверяем обязательные поля
-  const validationErrors = validateRequiredFields();
-  if (validationErrors.length > 0) {
-    console.error("Błędy walidacji:", validationErrors);
-    // Можно показать уведомление пользователю
-    alert("Wypełnij wszystkie wymagane pola:\n" + validationErrors.join("\n"));
-    return;
-  }
-
   emit("save", {
     fields: { ...fieldsData },
     packageFields: { ...packageFieldsData },
